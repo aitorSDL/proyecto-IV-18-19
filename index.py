aa1b422cebf674424d401e50ee9443bc808d5679
samuelac@correo.ugr.es <samuelac@correo.ugr.es>
2018-11-09 21:39:44 +0100

    añadiendo proyecto en heroku close #13

--- index.py
+++ index.py
@@ -1,5 +1,9 @@
 from flask import Flask
+
 app = Flask(__name__)
+PORT=5000
+DEBUG=False
+
 
 @app.route('/')
 def index():
@@ -8,3 +12,7 @@ def index():
 @app.route('/productos')
 def comparacion():
     return 'Hello, pepo!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port =  PORT, debug = DEBUG)
